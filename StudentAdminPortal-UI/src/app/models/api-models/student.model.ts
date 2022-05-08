a111835f35726690cac4d3d5f2590731e33de2ec
cyrus <cyrus_andrace14@yahoo.com>
2022-05-08 15:23:11 +0800

    end of section 3

---
+++ StudentAdminPortal-UI/src/app/models/api-models/student.model.ts
@@ -0,0 +1,16 @@
+import { StringMap } from "@angular/compiler/src/compiler_facade_interface";
+import { Address } from "./address.model";
+import { Gender } from "./gender.model";
+
+export interface Student{
+  id: string,
+  firstName: string,
+  lastName: string,
+  dateOfBirth: string,
+  email: string,
+  modile: number,
+profileImageUrl: string,
+genderId: string,
+gender: Gender,
+address: Address
+}
